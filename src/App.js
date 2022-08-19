fbd51740ac130cb5d29cdd68568fdfc39ac9c1d4
muhammadumar000 <FA20-BEE-150@ISBSTUDENT.COMSATS.EDU.PK>
2022-08-19 22:55:27 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Typography } from "@mui/material";
+import styled from "styled-components";
+import SalatTime from './Components/SalatTime'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Container>
+     
+      <Box>
+        <Typography pt={1} color="#FB8122" align="center" variant="h4">Prayer Time App</Typography>
+        <StyledLine />
+      </Box>
+      <SalatTime />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width:100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction:column;
+  background-color: #1D2228
+`
+const StyledLine = styled('hr')`
+  width:50rem;
+  margin-block:1rem;
+  background-color:#FB8122;
+  border: 1px solid  #FB8122;
+  @media (max-width:500px){
+    width:20rem
+  }
+`
---
+++ src/Components/SalatTime.js
@@ -0,0 +1,187 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import PlaceIcon from '@mui/icons-material/Place';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+
+export default function SalatTime() {
+
+  const [prayerTimeData, setPrayerTimeData] = React.useState(null);
+  const [cityName, setCityName] = React.useState("");
+  const [hour,setHour] = React.useState('');
+  const [minutes,setMinutes] = React.useState('');
+  const [seconds,setSeconds] = React.useState('');
+
+  const fetchPrayerTimes = async (name) => {
+    try {
+      const response = await fetch(
+        `https://muslimsalat.p.rapidapi.com/${name}.json`,
+        {
+          method: "GET",
+          headers: {
+            "X-RapidAPI-Key":
+              "e538a949f4mshb7b566383dcf0eep14bdcbjsnbc696cae1988",
+            "X-RapidAPI-Host": "muslimsalat.p.rapidapi.com",
+          },
+        }
+      );
+      const data = await response.json();
+      setPrayerTimeData(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const currentTime = () => {
+    const time = new Date();
+    setHour(time.getHours())
+    setMinutes(time.getMinutes())
+    setSeconds(time.getSeconds())
+  }
+
+  const searchHandler = () => {
+    if (cityName) {
+      fetchPrayerTimes(cityName);
+      setCityName("");
+    } else {
+      alert("Enter City Name");
+    }
+  };
+
+  React.useEffect(() => {
+    setTimeout(currentTime,1000)
+  })
+
+
+  React.useEffect(() => {
+    fetchPrayerTimes("kahuta");
+  }, []);
+
+
+
+  return (
+    <Box
+      flexDirection="column"
+      gap="1rem"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      paddingY='1rem'
+      bgcolor='#E1E2E2'
+      sx={{borderRadius:'10px',minWidth:{xs:'80%',sm:'40%'},margin:{xs:'1rem',sm:'unset'}}}
+    >
+      <Box
+        gap="1rem"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        sx={{flexDirection:{xs:'column',sm:'row'}}}
+      >
+        <TextField
+          id="outlined-basic"
+          label="City Name..."
+          variant="outlined"
+          onChange={(e) => setCityName(e.target.value)}
+          value={cityName}
+          autoFocus
+          sx={{ width: {sm: "22rem",xs:'18rem'},'&:active':{border:'none'},'input':{height:'1rem','&:hover':{outline:'none'}}, 'label':{color:'#1D2228','&:hover':{bgcolor:'red'}} }}
+        />
+        <Button
+          sx={{ height: "2.8rem" ,width:'10rem',bgcolor:'#FB8122',color:'#E1E2E2','&:hover':{bgcolor:'#1D2228'}}}
+          variant="contained"
+          onClick={searchHandler}
+        >
+          Search
+        </Button>
+      </Box>
+      <Grid mt={1} width='90%' container spacing={2}>
+        <Grid  item xs={6}>
+        {prayerTimeData && (
+          <Typography  variant="h6">City: {prayerTimeData.query}</Typography>
+        )}
+        </Grid>
+        <Grid  item xs={6}>
+        {prayerTimeData && (
+          <Typography  variant="h6">Date: {prayerTimeData.items[0].date_for}</Typography>
+        )}
+        </Grid>
+        <Grid  item xs={6}>
+        {prayerTimeData && (
+          <Typography  variant="h6">Sunrise: {prayerTimeData.items[0].shurooq}</Typography>
+        )}
+        </Grid>
+        <Grid  item xs={6}>
+        {prayerTimeData && (
+          <Typography variant="h6 sx={{fontSize:'1.2rem'}}">Time: {hour}:{minutes}:{seconds}</Typography>
+        )}
+        </Grid>
+      </Grid>
+      
+      <TableContainer
+        sx={{ width: {sm: "32rem",xs:'22rem'}, padding: "1rem", bgcolor:'#FB8122' }}
+        component={Paper}
+      >
+        <Table sx={{ width: {sm: 400,xs:300} }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell color='#1D2228' sx={{fontSize:'1.2rem'}}>Prayer</TableCell>
+              <TableCell color="#1D2228" sx={{fontSize:'1.2rem'}} align="left">Prayer Time</TableCell>
+            </TableRow>
+          </TableHead>
+          {prayerTimeData && (
+            <TableBody >
+              <TableRow >
+                <TableCell sx={{fontSize:'1.2rem'}} component="th" scope="row">
+                  fajr
+                </TableCell>
+                <TableCell sx={{fontSize:'1.2rem'}}l align="left">
+                  {prayerTimeData.items[0].fajr}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{fontSize:'1.2rem'}} component="th" scope="row">
+                  dhuhr
+                </TableCell>
+                <TableCell sx={{fontSize:'1.2rem'}} align="left">
+                  {prayerTimeData.items[0].dhuhr}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{fontSize:'1.2rem'}} component="th" scope="row">
+                  asr
+                </TableCell>
+                <TableCell sx={{fontSize:'1.2rem'}} align="left">
+                  {prayerTimeData.items[0].asr}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{fontSize:'1.2rem'}} component="th" scope="row">
+                  maghrib
+                </TableCell>
+                <TableCell sx={{fontSize:'1.2rem'}} align="left">
+                  {prayerTimeData.items[0].maghrib}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{fontSize:'1.2rem'}} component="th" scope="row">
+                  isha
+                </TableCell>
+                <TableCell sx={{fontSize:'1.2rem'}} align="left">
+                  {prayerTimeData.items[0].isha}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          )}
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
